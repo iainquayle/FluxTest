c2c12ff06b910f775a07a90569307fe2057cc236
iainquayle <36175014+iainquayle@users.noreply.github.com>
2022-11-24 18:52:08 -0700

    cat_channels explanation ++

--- src/FluxTest.jl
+++ src/FluxTest.jl
@@ -15,31 +15,35 @@ function create_data_loaders()
 	return train, test	
 end
 
-cat_channels(xy...) = cat(xy...; dims = Val(3))
+#equivalent to cat(x, y, z...; dims = 3)
+cat_channels_2d(xy...) = cat(xy...; dims = Val(3))
+#equivalent to cat(x, z...; dims = 2)
+cat_channels_1d(x...) = cat(x...; dims = Val(2))
 
 function conv_module_a(imgs_in, imgs_out)
 	return Chain(
+		Conv((3, 3), imgs_in=>imgs_in, relu, pad=SamePad()),
 		Conv((3, 3), imgs_in=>imgs_out, relu, pad=SamePad()),
 		Conv((3, 3), imgs_out=>imgs_out, relu, pad=SamePad()),
 	) 	
 end
 
-function shell_downsample(conv_mod, imgs_in_short, imgs_out_short, imgs_in_long, imgs_out_long)
-	short_conv = Conv((2, 2), imgs_in_short=>imgs_out_short, relu, pad = SamePad(), stride = (2, 2))
+function shell_downsample(conv_mod, imgs_pair_skip, imgs_pair_long) 
+	short_conv = Conv((2, 2), imgs_pair_skip, relu, pad = SamePad(), stride = (2, 2))
 	long_chain = Chain(
 		conv_mod,
-		Conv((2, 2), imgs_in_long=>imgs_out_long, relu, pad = SamePad(), stride = (2, 2)),
+		Conv((2, 2), imgs_pair_long, relu, pad = SamePad(), stride = (2, 2)),
 	)
-	return Parallel(cat_channels, short_conv, long_chain)
+	return Parallel(cat_channels_2d, short_conv, long_chain)
 end
 
 function create_inception()
 	return Chain(
 		Conv((5,5), 1=>8, relu), #24, 8
-		shell_downsample(conv_module_a(8, 8), 8, 4, 8, 12),#12, 16
+		shell_downsample(conv_module_a(8, 8), 8=>4, 8=>12),#12, 16
 		Dropout(0.25),
-		shell_downsample(conv_module_a(16, 16), 16, 6, 16, 26),#6, 32
-		shell_downsample(conv_module_a(32, 32), 32, 8, 32, 56),#3, 64
+		shell_downsample(conv_module_a(16, 16), 16=>6, 16=>26),#6, 32
+		shell_downsample(conv_module_a(32, 32), 32=>8, 32=>56),#3, 64
 		Dropout(0.25),
 		Flux.flatten,
 		Dense(3*3*64=>512, relu),
@@ -49,8 +53,34 @@ function create_inception()
 	)
 end
 
+
+
+
+function conv_mod_1d_a(imgs_in, imgs_out)
+	return Chain(
+		Conv((3), imgs_in=>imgs_out, pad=SamePad()),
+		Conv((3), imgs_in=>imgs_out, pad=SamePad()),
+		Conv((3), imgs_in=>imgs_out, pad=SamePad())
+	)	
+end
+
+function shell_conv_1d(conv_module, imgs_pair_skip, imgs_pair_long)
+	skip_conv = Conv((3, 1), imgs_pair_skip, relu, pad = SamePad(), stride = (2, 1))
+	long = Chain(
+		conv_mod_1d_a,
+		Conv((3, 1), imgs_pair_long, relu, pad = SamePad(), stride = (2, 1))
+	)
+	return Parallel(cat_channels_1d, skip_conv, long)
+end
+
+function create_finance_algo()
+	return Chain(
+
+	)
+end
+
+
 function create_lenet()
-	#TODO: switch back to no padding
 	return Chain(
 		Conv((5, 5), 1=>6, relu), #24 
 		MaxPool((2, 2)), #12
@@ -62,6 +92,11 @@ function create_lenet()
 		Dense(64=>10),
 	)
 end
+
+
+
+
+
 
 function eval_accuracy(loader, model, device)
 	loss = 0f0
@@ -78,6 +113,8 @@ function eval_accuracy(loader, model, device)
 	return (loss = loss / num_total, accuracy = accuracy / num_total)
 end
 
+
+
 function train()
 	@info "creating data loaders"
 	train_loader, test_loader = create_data_loaders()
@@ -89,21 +126,18 @@ function train()
 
 	parameters = Flux.params(model)
 	#optimizer = ADAMW(0.003, (.85, .99), 0.01)
-	optimizer = ADAM(0.0003)
-
+	optimizer = Adam(0.0005)
+	loss(x, y) = Flux.logitcrossentropy(model(x), y)
+	
 	@info "beginning training"
-	for i in 1:1000
+	for i in 1:100
 		for (x, y) in train_loader
 			x, y = x |> gpu, y |> gpu
-			calculated_grad = Flux.gradient(parameters) do 
-				y_hat = model(x)
-				Flux.logitcrossentropy(y_hat, y)
-			end	
-
+			calculated_grad = Flux.gradient(() -> loss(x, y), parameters) 
 			Flux.Optimise.update!(optimizer, parameters, calculated_grad)
 		end
 
-		if i % 10 == 0
+		if i % 1 == 0
 			train_accuracy = eval_accuracy(train_loader, model, gpu)
 			test_accuracy = eval_accuracy(test_loader, model, gpu)
 			@info "epoch: $(i), train accuracy: $(train_accuracy), test accuracy: $(test_accuracy)"
@@ -115,5 +149,4 @@ end
 CUDA.versioninfo()
 train()
 
-#	println(size(Flux.params(shell_downsample(conv_module_a(8, 8), 8, 8, 8, 8))[:,:,:,:]))#12, 16
-end # module
+end 
